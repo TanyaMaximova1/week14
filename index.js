0b7dac0c8bdcbcf460ddb19552e6b612b7ec6ab2
TanyaMaximova1 <tatamurom@gmail.com>
2023-01-08 22:55:23 +0300

    step

---
+++ index.js
@@ -0,0 +1,15 @@
+// задание 2
+
+//сложение
+// function sum(a, b) {
+//     return a + b;
+// }
+// let result = sum(firstNumber1, secondNumber1);
+// alert(result);
+
+
+console.log(foo);
+
+var foo = "Cat";
+
+console.log(foo);
